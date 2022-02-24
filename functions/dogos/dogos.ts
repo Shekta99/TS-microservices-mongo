cf3684e8f099eeaf1cfe8067c6b8cdef1da44e72
Sergio Marentes <sergiomarentes705@gmail.com>
2022-02-24 08:55:45 -0500

    add post support

--- functions/dogos/dogos.ts
+++ functions/dogos/dogos.ts
@@ -1,10 +1,14 @@
 import { Handler } from "@netlify/functions";
+import { createDogos } from "./createDogos";
 import { readDogos } from "./readDogos";
 
 const handler: Handler = (event, context, callback) => {
   switch (event.httpMethod) {
     case "GET":
       return readDogos(event, context, callback);
+      break;
+    case "POST":
+      return createDogos(event, context, callback);
       break;
   }
 
